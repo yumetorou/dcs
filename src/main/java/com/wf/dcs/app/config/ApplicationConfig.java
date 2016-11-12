a9006f9cb737367bc01db73d1dca37e9b32f9768
Dennis De Vera Jr <ddevera@exist.com>
2016-11-12 22:35:55 +0800

    add json converter to standardize data return

--- src/main/java/com/wf/dcs/app/config/ApplicationConfig.java
+++ src/main/java/com/wf/dcs/app/config/ApplicationConfig.java
@@ -1,9 +1,20 @@
 package com.wf.dcs.app.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.wf.dcs.app.repository.base.impl.BaseJpaRepositoryImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 /**
  * @author ddevera
@@ -12,5 +23,33 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableJpaRepositories(repositoryBaseClass = BaseJpaRepositoryImpl.class,
         basePackages = "com.wf.dcs.app.repository", repositoryImplementationPostfix = "CustomImpl")
-public class ApplicationConfig {
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(jsonConverter());
+        converters.add(byteArrayConverter());
+    }
+
+    /**
+     * Json marshalling configuration.
+     *
+     * @return the {@link org.springframework.http.converter.json.MappingJackson2HttpMessageConverter}
+     */
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.getObjectMapper().registerModule(new JodaModule())
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        converter.getObjectMapper().registerModule(new Hibernate4Module());
+
+        return converter;
+    }
+
+
+    @Bean
+    public ByteArrayHttpMessageConverter byteArrayConverter() {
+        return new ByteArrayHttpMessageConverter();
+    }
 }
